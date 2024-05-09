8c0d0022902601d8dafb2a6ac3fb89484cb1e1fd
Sayan Das <sayandas61668@gmail.com>
2024-05-09 10:21:08 +0530

    03

--- src/components/Installation.jsx
+++ src/components/Installation.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Installation = () => {
   return (
-    <div className=" flex flex-col justify-center items-center">
+    <div className="relative z-10 flex flex-col justify-center items-center">
       <h1 className=" leading-tight inline-block font-bold text-[58px] text-transparent bg-clip-text bg-gradient-to-r from-[#f349c0] via-[#565dde] to-[#15a39c]"> Downlode Our Software</h1>
       <a
         href="https://github.com/Souvik9205/HandGesture-oparater
-
-"
+        "
+        classname="cursor-pointer"
       >
         <div className=" text-white my-16 bg-[#228b33] py-2 px-5 rounded-md">GitHub</div>
       </a>
